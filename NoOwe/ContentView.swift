4ae9dab0268c56f92f7f1113c812a1f84b989876
filipwicha <5021567@gmail.com>
2019-12-02 15:55:51 +0100

    Initial Commit

---
+++ NoOwe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoOwe
+//
+//  Created by Filip Wicha on 02/12/2019.
+//  Copyright © 2019 Filip Wicha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
