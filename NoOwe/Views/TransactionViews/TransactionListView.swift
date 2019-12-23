c37a69e34d18ae48595cc54667a93bd247b41237
filipwicha <33604199+filipwicha@users.noreply.github.com>
2019-12-23 14:39:12 +0100

    reverted card view

--- NoOwe/Views/TransactionViews/TransactionListView.swift
+++ NoOwe/Views/TransactionViews/TransactionListView.swift
@@ -24,135 +24,114 @@ struct TransactionListView: View {
     }
     
     var body: some View {
-        List{
-            Section{
-                VStack{
-                    Text("Summary")
-                    ForEach(self.transactionListVM.budgetMemberListVM.budgetMembers){ budgetMember in
-                        HStack {
-                            Text(budgetMember.nickname)
-                            Text("\(self.getTotalOwe(budgetMemberId: budgetMember.id), specifier: "%.2f") \(self.currencyDownload.currencies.filter {$0.id == self.budget.currencyId}[0].code)")
-                        }
-                    }
-                }
-            }
-            Section{
-                ForEach(self.transactionListVM.transactions){ transaction in
-                    ZStack{
-                        Rectangle()
-                            .fill(.gray)
-                        
-                        VStack {
-                            Image(self.transactionListVM.categories.filter { $0.id == transaction.categoryId }[0].photo)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                            
+        GeometryReader { geometry in
+            List{
+                Section{
+                    VStack{
+                        Text("Summary")
+                        ForEach(self.transactionListVM.budgetMemberListVM.budgetMembers){ budgetMember in
                             HStack {
-                                VStack(alignment: .leading) {
-                                    Text("\(self.transactionListVM.getTransactionTotal(id: transaction.id), specifier: "%.2f") \(self.currencyDownload.currencies.filter {$0.id == self.budget.currencyId}[0].code)")
-                                        .font(.headline)
-                                        .foregroundColor(.secondary)
-                                    Text(transaction.title)
-                                        .font(.title)
-                                        .fontWeight(.black)
-                                        .foregroundColor(.primary)
-                                        .lineLimit(3)
-                                    Text(self.transactionListVM.categories.filter { $0.id == transaction.categoryId }[0].photo)
-                                        .font(.caption)
-                                        .foregroundColor(.secondary)
-                                }
-                                .layoutPriority(100)
-                                
-                                Spacer()
+                                Text(budgetMember.nickname)
+                                Text("\(self.getTotalOwe(budgetMemberId: budgetMember.id), specifier: "%.2f") \(self.currencyDownload.currencies.filter {$0.id == self.budget.currencyId}[0].code)")
                             }
-                            .padding()
                         }
-                        .cornerRadius(10)
-                        //                    .overlay(
-                        //                        RoundedRectangle(cornerRadius: 10)
-                        //                            .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), linewidth: 1)
-                        //                    )
-                        //                        .padding([.top, .horizontal])
                     }
                 }
+                Section{
+                    Button(action:{
+                        self.showNewTransactionModal()
+                    }){
+                        HStack{
+                            Spacer()
+                            Image(systemName: "plus").resizable().frame(width: 30, height: 30)
+                        }
+                        .padding(12)
+                    }.frame(width: geometry.size.width-30, height: 50)
+                    
+                    ForEach(self.transactionListVM.transactions){ transaction in
+                        HStack(alignment: .center){
+                            ZStack(alignment: .leading){
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(self.getColor(colorString: self.budget.color), lineWidth: 10)
+                                
+                                HStack {
+                                    Text(transaction.title).font(.headline).padding(20)
+                                    Spacer()
+                                    VStack{
+                                        Text("\(self.transactionListVM.getTransactionTotal(id: transaction.id), specifier: "%.2f") \(self.currencyDownload.currencies.filter {$0.id == self.budget.currencyId}[0].code)").font(.title).bold().padding(20)
+                                        //  Image(systemName: transaction.categoryId)
+                                    }
+                                }
+                            }.frame(width: geometry.size.width-30, height: 140)
+                        }
+                    }
+                }
+                
+            }.sheet(isPresented: self.$showModal){
+                CreateNewTransactionView(budget: self.budget, budgetMemberListVM: self.transactionListVM.budgetMemberListVM, currencySymbol: self.currencyDownload.currencies.filter {$0.id == self.budget.currencyId}[0].code, categories: self.transactionListVM.categories)
             }
-            
-        }.sheet(isPresented: self.$showModal){
-            CreateNewTransactionView(budget: self.budget, budgetMemberListVM: self.transactionListVM.budgetMemberListVM, currencySymbol: self.currencyDownload.currencies.filter {$0.id == self.budget.currencyId}[0].code, categories: self.transactionListVM.categories)
-        }
-            
-        .navigationBarTitle("\(self.budget.name)")
-        .navigationBarItems(trailing:
-            HStack{
+                
+            .navigationBarTitle("\(self.budget.name)")
+            .navigationBarItems(trailing:
                 Button(action:{
                     self.transactionListVM.fetchTransactions()
                 }){
                     Image(systemName: "arrow.clockwise").foregroundColor(Color.white)
                 }
-                Button(action:{
-                self.showNewTransactionModal()
-                }){
-                    HStack{
-                        Spacer()
-                        Image(systemName: "plus").resizable().frame(width: 30, height: 30)
+            )
+        }
+    }
+        func setNavigationBarColor(colorString: String){
+            UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
+            UINavigationBar.appearance().backgroundColor = getColor(colorString: colorString)
+        }
+        
+        func getColor(colorString: String) -> UIColor {
+            let hueArray: [String] = colorString.components(separatedBy: ",")
+            let r, g, b: CGFloat
+            
+            r = CGFloat(Double(hueArray[0])!/Double(255))
+            g = CGFloat(Double(hueArray[1])!/Double(255))
+            b = CGFloat(Double(hueArray[2])!/Double(255))
+            
+            return UIColor(red: r, green: g, blue: b, alpha: 1)
+        }
+        
+        func showNewTransactionModal() {
+            self.showModal = true
+        }
+        
+        func getColor(colorString: String) -> Color {
+            let hueArray: [String] = colorString.components(separatedBy: ",")
+            let full = Double(255)
+            
+            let r = Double(hueArray[0])!/full
+            let g = Double(hueArray[1])!/full
+            let b = Double(hueArray[2])!/full
+            
+            return Color(red: r, green: g, blue: b)
+        }
+        
+        func getTotalOwe(budgetMemberId: Int) -> Double{
+            var owe: Double = 0.0
+            self.transactionListVM.transactions.forEach { transaction in
+                transaction.shares.forEach { share in
+                    if share.member_id == budgetMemberId {
+                        owe += share.amount
                     }
                 }
             }
-        )
-        
-    }
-    
-    func setNavigationBarColor(colorString: String){
-        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
-        UINavigationBar.appearance().backgroundColor = getColor(colorString: colorString)
-    }
-    
-    func getColor(colorString: String) -> UIColor {
-        let hueArray: [String] = colorString.components(separatedBy: ",")
-        let r, g, b: CGFloat
-        
-        r = CGFloat(Double(hueArray[0])!/Double(255))
-        g = CGFloat(Double(hueArray[1])!/Double(255))
-        b = CGFloat(Double(hueArray[2])!/Double(255))
-        
-        return UIColor(red: r, green: g, blue: b, alpha: 1)
-    }
-    
-    func showNewTransactionModal() {
-        self.showModal = true
-    }
-    
-    func getColor(colorString: String) -> Color {
-        let hueArray: [String] = colorString.components(separatedBy: ",")
-        let full = Double(255)
-        
-        let r = Double(hueArray[0])!/full
-        let g = Double(hueArray[1])!/full
-        let b = Double(hueArray[2])!/full
-        
-        return Color(red: r, green: g, blue: b)
-    }
-    
-    func getTotalOwe(budgetMemberId: Int) -> Double{
-        var owe: Double = 0.0
-        self.transactionListVM.transactions.forEach { transaction in
-            transaction.shares.forEach { share in
-                if share.member_id == budgetMemberId {
-                    owe += share.amount
-                }
-            }
+            
+            return owe
         }
-        
-        return owe
     }
-}
-
-
-
-#if DEBUG
-struct TransactionListView_Previews: PreviewProvider {
-    static var previews: some View {
-        TransactionListView(budget: BudgetViewModel(budget: Budget(id: 4, name: "lkk", color: "2222", owner_id: 1, currency_id: 1)))
+    
+    
+    
+    #if DEBUG
+    struct TransactionListView_Previews: PreviewProvider {
+        static var previews: some View {
+            TransactionListView(budget: BudgetViewModel(budget: Budget(id: 4, name: "lkk", color: "2222", owner_id: 1, currency_id: 1)))
+        }
     }
-}
 #endif
